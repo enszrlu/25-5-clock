a8a13dc80a7d76f07feba2e572f29818b28cb2ab
enszrlu <enes.zrl@gmail.com>
2022-02-17 01:00:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Timer from './components/Timer.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionLength: 7,
+      breakLength: 5,
+      onSession: true
+    }
+    this.handleTimerEnd = this.handleTimerEnd.bind(this);
+  }
+
+  handleTimerEnd(event) {
+    if (this.state.onSession) {
+      this.setState({onSession: false})
+      
+    }
+    else {
+      this.setState({onSession: true})
+      console.log('test')
+    }
+  }
+
+
+  render() {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + (this.state.onSession ? this.state.sessionLength : this.state.breakLength)); // 10 minutes timer
+    return (
+      <div className="App">
+        <Timer expiryTimestamp={time} expiryFunction={this.handleTimerEnd} timerDuration={this.state.onSession ? this.state.sessionLength : this.state.breakLength}/>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
