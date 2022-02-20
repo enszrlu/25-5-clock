4fc94a66d2022b210c5846b00a692d1a113f7cc8
enszrlu <enes.zrl@gmail.com>
2022-02-20 22:15:02 +0300

    completed

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,32 +3,34 @@ import { VscDebugPause, VscDebugStart, VscDebugRestart} from 'react-icons/vsc';
 
 export default function Timer(props) {
     return (
-      <div>
+      <div id="timer">
         <h1>25 + 5 Clock </h1>
-        <div>
-            <div>
+        <div id="lenght-cont">
+            <div className='length-sub-cont'>
                 <h2 id="break-label">Break Length</h2>
-                <div>
-                    <button id="break-increment" onClick={props.increaseBreakLength}><AiOutlineArrowUp /></button>
-                    <h2 id="break-length">{props.breakLength}</h2>
-                    <button id="break-decrement" onClick={props.decreaseBreakLength}><AiOutlineArrowDown /></button>
+                <div className='lenght-controllers'>
+                    <button className='length-button' id="break-increment" onClick={props.increaseBreakLength}><AiOutlineArrowUp /></button>
+                    <h2 id="break-length" className='length-label'>{props.breakLength}</h2>
+                    <button className='length-button' id="break-decrement" onClick={props.decreaseBreakLength}><AiOutlineArrowDown /></button>
                 </div>
             </div>
-            <div>
+            <div className='length-sub-cont'>
                 <h2 id="session-label">Session Length</h2>
-                <div>
-                    <button id="session-increment" onClick={props.increaseSessionLength}><AiOutlineArrowUp /></button>
-                    <h2 id="session-length">{props.sessionLength}</h2>
-                    <button id="session-decrement" onClick={props.decreaseSessionLength}><AiOutlineArrowDown /></button>
+                <div className='lenght-controllers'>
+                    <button className='length-button' id="session-increment" onClick={props.increaseSessionLength}><AiOutlineArrowUp /></button>
+                    <h2 id="session-length" className='length-label'>{props.sessionLength}</h2>
+                    <button className='length-button' id="session-decrement" onClick={props.decreaseSessionLength}><AiOutlineArrowDown /></button>
                 </div>
             </div>
         </div>
-        <div className={props.status}>
+        <div id="timer-div" className={props.status}>
             <h2 id="timer-label">{props.state}</h2>
             <div id="time-left"><span>{props.minutes < 10 ? "0"+props.minutes : props.minutes}</span>:<span>{props.seconds < 10 ? "0"+props.seconds : props.seconds}</span></div>
         </div>
-        <button onClick={props.status == "Pause" ? props.start : props.pause} id="start_stop"> <VscDebugStart /> <VscDebugPause /></button>
-        <button onClick={props.restart} id="reset"><VscDebugRestart /></button>
+        <div id="action-buttons-div">
+            <button onClick={props.status == "Pause" ? props.start : props.pause} id="start_stop"> <VscDebugStart /> <VscDebugPause /></button>
+            <button onClick={props.restart} id="reset"><VscDebugRestart /></button>
+        </div>
         <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
       </div>
     );
